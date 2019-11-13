460e535de1819ef5202430316405ae833ff86712
André Bedran <affbedran@gmail.com>
2019-11-13 20:40:55 -0300

    Created response models for the imgur api. Also added a disposeBag to base viewController class.

---
+++ KittenProject/KittenProject/Models/Gallery.swift
@@ -0,0 +1,13 @@
+//
+//  Gallery.swift
+//  KittenProject
+//
+//  Created by André Felipe Fleck Bedran on 13/11/19.
+//  Copyright © 2019 André Bedran. All rights reserved.
+//
+
+import Foundation
+
+class Gallery: Codable {
+    let images: [Image]?
+}
---
+++ KittenProject/KittenProject/Models/Image.swift
@@ -0,0 +1,14 @@
+//
+//  Image.swift
+//  KittenProject
+//
+//  Created by André Felipe Fleck Bedran on 13/11/19.
+//  Copyright © 2019 André Bedran. All rights reserved.
+//
+
+import Foundation
+
+class Image: Codable {
+    let type: String?
+    let link: String?
+}
